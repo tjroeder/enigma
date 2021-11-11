3354d572701eff412e09ee663e688e0b648d89a9
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-10 20:29:08 -0700

    Feat: created FileIO methods

--- mod/file_io.rb
+++ mod/file_io.rb
@@ -1,9 +1,10 @@
 module FileIO
-  def file_read_in(file_path)
-
+  def file_read(file_path)
+    file_data = IO.read(file_path)
+    file_data.downcase
   end
 
-  def file_write_in(file_path)
-    
+  def file_write_out(new_message, file_path)
+    File.write(file_path.to)
   end
 end
