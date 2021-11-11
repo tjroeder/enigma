11d877a5af001a1a5b1f4e892f93797b3ee3756a
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-10 18:17:45 -0700

    Feat: created attributes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,13 @@
+require_relative '../mod/file_io'
+
 class Enigma
-  
+  include FileIO
+
+  attr_reader :message, :key, :offset
+
+  def initialize
+    @message = ''
+    @key = ''
+    @offset = ''
+  end
 end
