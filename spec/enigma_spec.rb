92632e5c203fb399f2ce828973a6b219aa93fe4c
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-10 20:48:19 -0700

    Test: created #file_write test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -3,6 +3,8 @@ require_relative '../lib/enigma'
 
 RSpec.describe Enigma do
   let(:fixture_message_path) { './spec/fixtures/message.txt' }
+  let(:fixture_encrypted_path) { './spec/fixtures/encrypted.txt' }
+  let(:fixture_decrypted_path) { './spec/fixtures/decrypted.txt' }
   let(:enigma) { Enigma.new }
   
   describe '#initialize' do
@@ -18,10 +20,20 @@ RSpec.describe Enigma do
   end
 
   describe 'file_io module' do
-    describe '#file_read_in' do
+    describe '#file_read' do
       it 'can read a file name and convert to downcase string' do
         expected = "this is a string for testing\nthis is a second line\nth1s line h4$ `1234567890-=[]\\;',./~!@\#$%^&*()_+{}|:\"<>? special characters\nthis is the final line!"
+
         expect(enigma.file_read(fixture_message_path)).to eq(expected)
+      end
+    end
+
+    describe '#file_write' do
+      it 'can write a new message to a file name' do
+        new_message = "this is a string for testing\nthis is a second line\nth1s line h4$ `1234567890-=[]\\;',./~!@\#$%^&*()_+{}|:\"<>? special characters\nthis is the final line!"
+        enigma.file_write(fixture_decrypted_path, new_message)
+
+        expect(enigma.file_read(fixture_decrypted_path)).to eq(new_message)
       end
     end
   end
