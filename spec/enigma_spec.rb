6ca3eb15f4dc303230073eb3c5d019656a2c282a
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-10 18:13:04 -0700

    Test: created class initalize test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,12 @@
 require_relative './spec_helper'
+require_relative '../lib/enigma'
 
 RSpec.describe Enigma do
+  let(:enigma) { Enigma.new }
   
+  describe '#initialize' do
+    it 'exists' do
+      expect(enigma).to be_a(Enigma)
+    end
+  end
 end
