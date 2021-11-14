92adf6515f38495d66b855aa1bf5fea907ed5677
Tim Roeder <78194232+tjroeder@users.noreply.github.com>
2021-11-14 15:12:35 -0700

    Feat: updated for #cli_user_input changes

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,12 +1,8 @@
 require_relative './enigma'
 
 enigma = Enigma.new
-message_path = ARGV[0]
-encrypt_path = ARGV[1]
-message = enigma.file_read(message_path)
-enigma.file_write_path = encrypt_path
+enigma.cli_user_input(ARGV)
+encrypted_hash = enigma.encrypt(enigma.cli_message)
+enigma.file_write(enigma.cli_write_path, encrypted_hash[:encryption])
 
-encrypted_hash = enigma.encrypt(message)
-enigma.file_write(encrypt_path, encrypted_hash[:encryption])
-
-puts enigma.print_message
+puts enigma.cli_print_message
